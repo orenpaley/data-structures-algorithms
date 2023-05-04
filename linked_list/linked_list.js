6ab5f60a52b35ffb49bcfaf24c01bc6915448340
Oren Paley <oepaley@gmail.com>
2023-05-03 21:58:18 -0500

    finished linked list

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -5,16 +5,16 @@
 */
 
 class Node {
-  constructor(val, next=null) {
+  constructor(val, next = null) {
     this.val = val;
-    this.next = next
+    this.next = next;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = null
-    this.tail = null
+    this.head = null;
+    this.tail = null;
   }
 
   // traverse linked list
@@ -22,7 +22,7 @@ class LinkedList {
     let current = this.head;
     while (current) {
       console.log(current.val);
-      current = current.next
+      current = current.next;
     }
   }
 
@@ -30,7 +30,7 @@ class LinkedList {
   find(val) {
     let current = this.head;
 
-    while(current) {
+    while (current) {
       if (current.val === val) return true;
       current = current.next;
     }
@@ -38,8 +38,8 @@ class LinkedList {
   }
 
   // add a new node to end of linked list
-  append(val) {
-    val = new Node(val)
+  push(val) {
+    val = new Node(val);
     if (!this.head) {
       this.head = val;
       this.tail = val;
@@ -47,16 +47,102 @@ class LinkedList {
     // set the next pointer of tail to new node
     this.tail.next = val;
     // set the tail to be the new node
-    this.tail = val;
+    this.tail.val = val;
+  }
+
+  unshift(val) {
+    val = new Node(val);
+    if (!this.head) {
+      this.head = val;
+      this.tail = val;
+    }
+    this.head.next = this.head;
+    this.head.val = val;
+  }
+
+  pop() {
+    if (!this.head) throw new Error("nothing in list");
+    current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    let tmp = current.next;
+    current.next = null;
+    this.tail = current;
+    return tmp;
+  }
+
+  shift() {
+    if (!this.head) throw new Error("nothing in list");
+    let tmp = this.head;
+    this.head = this.head.next;
+    return tmp;
+  }
+
+  getAt(idx) {
+    if (!this.head) throw new Error("nothing in list");
+    let current = this.head;
+    let steps = 0;
+    while (current) {
+      while (steps < idx) {
+        current = current.next;
+        steps++;
+      }
+      return current.val;
+    }
+    throw new Error("index out of range");
+  }
+  setAt(idx, val) {
+    if (!this.head) throw new Error("nothing in list");
+    let current = this.head;
+    let steps = 0;
+    while (current) {
+      while (steps < idx) {
+        current = current.next;
+        steps++;
+      }
+      current.val = val;
+    }
+    throw new Error("index out of range");
+  }
+  insertAt(idx, val) {
+    val = new Node(val);
+    if (!this.head) throw new Error("nothing in list");
+    let current = this.head;
+    let steps = 0;
+    while (current) {
+      while (steps < idx) {
+        current = current.next;
+        steps++;
+      }
+      let tmp = current;
+      current.val = val;
+      current.next = tmp;
+    }
+    throw new Error("index out of range");
+  }
+  removeAt(idx) {
+    if (!this.head) throw new Error("nothing in list");
+    let current = this.head;
+    let steps = 0;
+    while (current) {
+      while (steps + 1 < idx) {
+        current = current.next;
+        steps++;
+      }
+      let tmp = current;
+      current.next = current.next.next;
+      return tmp.val;
+    }
+    throw new Error("index out of range");
   }
 }
 
-const history = new LinkedList()
-history.append(('alpha'));
-history.append(('beta'));
-history.append(('charlie'));
-history.append(('delta'));
+const history = new LinkedList();
+history.append("alpha");
+history.append("beta");
+history.append("charlie");
+history.append("delta");
 history.traverse();
-console.log(history.find('alpha'));
-console.log(history.find('amazon.com'));
-
+console.log(history.find("alpha"));
+console.log(history.find("amazon.com"));
